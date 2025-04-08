02f825a009da9825037a344e355227c7f2f03485
TactlessSet <lbmilam4637@gmail.com>
2025-04-08 08:53:02 -0500

    Update Health.cs

--- GameDevGame/Assets/Health.cs
+++ GameDevGame/Assets/Health.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class Health : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float health = 100;
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+
     void Update()
     {
         
+    }
+    
+    public float NewHealth(float curHealth, float damage)
+    {
+        float newHealth = curHealth - damage;
+        return newHealth;
     }
 }
